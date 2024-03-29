2a45c08fd1073c4dbf3a634550ab91dcc87e1542
Timon K John <timonkjohn@gmail.com>
2024-03-30 00:08:23 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/stash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:stash/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stash',
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    const bgColor = Color.fromRGBO(232, 218, 229, 1);
+    return const Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: bgColor,
+      body: HomePage(),
+    );
+  }
+}
